24eff0463127383f63ed6eb52cd2df96beb7191a
Gideon Goldberg <gidsgoldberg@gmail.com>
2016-08-22 17:43:57 +0100

    Fetch fields from Google Sheet

---
+++ src/js/lib/sheettodom.js
@@ -0,0 +1,25 @@
+import sheetUrl from './sheetURL'
+import reqwest from 'reqwest'
+
+
+export default function sheettodom(sheetID, sheetName, el, callback) {
+
+    var sheet = (sheetUrl(sheetID));
+
+    reqwest({
+        'url': sheet,
+        'type': 'json',
+        'crossOrigin': true,
+        'success': resp => {
+
+            //get list of elements with data-sheet-attribute
+            for (const node of el.querySelectorAll('[data-sheet-attribute]')) {
+                const value = node.getAttribute('data-sheet-attribute');
+                node.innerHTML = resp.sheets[sheetName][0][value];
+            }
+            callback()
+
+        }
+    });
+
+}
--- src/js/lib/youtube.js
+++ src/js/lib/youtube.js
@@ -1,13 +1,11 @@
 import youTubeIframe from 'youtube-iframe-player'
 import reqwest from 'reqwest'
 
-export function pimpYouTubePlayer(videoId, placeholderId, height, width) {
+export function pimpYouTubePlayer(videoId, node, height, width) {
     youTubeIframe.init(function() {
         //preload youtube iframe API
-        const p = document.getElementById(placeholderId);
-
-        p.addEventListener('click', function() {
-            var youTubePlayer = youTubeIframe.createPlayer(placeholderId, {
+        node.addEventListener('click', function() {
+            var youTubePlayer = youTubeIframe.createPlayer(node, {
                 height: height,
                 width: width,
                 videoId: videoId,
@@ -32,9 +30,10 @@ function getYouTubeVideoDuration(videoId, callback){
         url: 'https://www.googleapis.com/youtube/v3/videos?part=contentDetails&id=' + videoId + '&key=' + apiKey,
         type: 'json',
         crossOrigin: true,
-        success: (resp) => {let duration =  resp.items[0].contentDetails.duration;
-                            let re = /PT(\d+)M(\d+)S/;
-                            callback(duration.replace(re,'$1:$2'))}
+        success: (resp) => {
+            let duration =  resp.items[0].contentDetails.duration;
+            let re = /PT(\d+)M(\d+)S/;
+            callback(duration.replace(re,'$1:$2'))}
     });
 }
 
