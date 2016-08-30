55b96a18fe4d59eb8f1ffce048a39322beaac11f
Gideon Goldberg <gidsgoldberg@gmail.com>
2016-08-30 16:48:41 +0100

    use es6 template strings

--- src/js/lib/emailsignupURL.js
+++ src/js/lib/emailsignupURL.js
@@ -1,3 +1,3 @@
 export default function emailsignupURL(listId) {
-    return  window.location.origin === 'https://www.theguardian.com' ? 'https://www.theguardian.com/email/form/plaindark/' + listId : 'https://m.code.dev-theguardian.com/email/form/plaindark/' + listId;
+    return window.location.origin === 'https://www.theguardian.com' ? `https://www.theguardian.com/email/form/plaindark/${listId}` : `https://m.code.dev-theguardian.com/email/form/plaindark/${listId}`;
 }
