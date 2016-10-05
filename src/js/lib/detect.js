bbfa7ee4f1656759e12c50b7b30275047c56eccb
Akash A <akash1810@gmail.com>
2016-10-05 12:49:45 +0100

    refactor isMobile function
    Extract it into its own module, keeps youtube.js clean.

--- src/js/lib/detect.js
+++ src/js/lib/detect.js
@@ -1,2 +1,8 @@
-let isTouchscreenDevice = (('ontouchstart' in window) || (navigator.MaxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0));
-export {isTouchscreenDevice};
+function isMobile() {
+    const isIOS = () => /(iPad|iPhone|iPod touch)/i.test(navigator.userAgent);
+    const isAndroid = () => /Android/i.test(navigator.userAgent);
+
+    return isIOS() || isAndroid();
+}
+
+export { isMobile };
