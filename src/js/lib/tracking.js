54322b058d6e9a8600de7b43f071578cad1af7a2
Akash A <akash1810@gmail.com>
2016-10-14 12:12:38 +0100

    crude polyfill for object.assign
    couldn't get babel plugin working, so just using the polyfill provided by mdn (https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Polyfill)

--- src/js/lib/tracking.js
+++ src/js/lib/tracking.js
@@ -11,6 +11,31 @@ class Tracker {
     }
 
     buildGoogleAnalyticsEventObject(event) {
+        if (typeof Object.assign != 'function') {
+            (function () {
+                Object.assign = function (target) {
+                    'use strict';
+                    // We must check against these specific cases.
+                    if (target === undefined || target === null) {
+                        throw new TypeError('Cannot convert undefined or null to object');
+                    }
+
+                    var output = Object(target);
+                    for (var index = 1; index < arguments.length; index++) {
+                        var source = arguments[index];
+                        if (source !== undefined && source !== null) {
+                            for (var nextKey in source) {
+                                if (source.hasOwnProperty(nextKey)) {
+                                    output[nextKey] = source[nextKey];
+                                }
+                            }
+                        }
+                    }
+                    return output;
+                };
+            })();
+        }
+
         // copy `baseEventObject`
         const eventObject = Object.assign({}, this.googleAnalyticsProperties.baseEventObject);
         eventObject[this.googleAnalyticsProperties.metricMap[event]] = 1;
