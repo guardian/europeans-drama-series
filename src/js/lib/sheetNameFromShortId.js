235096bedb28c39cd2b396b120c3573a55d67f86
killa_bytes <lisa.villani@theguardian.com>
2017-06-20 11:19:04 +0100

    Updating fallback url (#131)
    Changing fallback url from the-infamous-chalk-girl to fish-story

--- src/js/lib/sheetNameFromShortId.js
+++ src/js/lib/sheetNameFromShortId.js
@@ -1,4 +1,4 @@
-const DEFAULT_DOC = 'the-infamous-chalk-girl';
+const DEFAULT_DOC = 'fish-story';
 
 export default function sheetNameFromShortId(docsArray, shortUrl) {
     const map = new Map(docsArray);
