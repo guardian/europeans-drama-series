a95e935c8a485afa9bab1b2ad1cc5a99ffe16ef3
killa_bytes <lisa.villani@theguardian.com>
2017-07-17 12:46:04 +0100

    Adding dearborn-michigan as fallback url
    Adding dearborn-michigan as fallback url

--- src/js/lib/sheetNameFromShortId.js
+++ src/js/lib/sheetNameFromShortId.js
@@ -1,4 +1,4 @@
-const DEFAULT_DOC = 'pitching-up';
+const DEFAULT_DOC = 'dearborn-michigan';
 
 export default function sheetNameFromShortId(docsArray, shortUrl) {
     const map = new Map(docsArray);
