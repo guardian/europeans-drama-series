fe46ea50d22b9af61fb67f556fc7deda68a34bee
killa_bytes <lisa.villani@theguardian.com>
2017-12-04 13:22:43 +0000

    Changes fallback to killing-gävle
    Adding ä

--- src/js/lib/sheetNameFromShortId.js
+++ src/js/lib/sheetNameFromShortId.js
@@ -1,4 +1,4 @@
-const DEFAULT_DOC = 'killing-gavle';
+const DEFAULT_DOC = 'killing-gävle';
 
 export default function sheetNameFromShortId(docsArray, shortUrl) {
     const map = new Map(docsArray);
