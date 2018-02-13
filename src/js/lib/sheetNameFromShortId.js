0c3fd69b228e933e03ecc5e6f84b092f1d5e1449
killa_bytes <lisa.villani@theguardian.com>
2018-02-13 10:33:08 +0000

    Adds new fallback url

--- src/js/lib/sheetNameFromShortId.js
+++ src/js/lib/sheetNameFromShortId.js
@@ -1,4 +1,4 @@
-const DEFAULT_DOC = 'conectifai';
+const DEFAULT_DOC = 'how-to-make-a-pearl';
 
 export default function sheetNameFromShortId(docsArray, shortUrl) {
     const map = new Map(docsArray);
