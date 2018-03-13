54927ca8a0ebefd33efbb95cea4b3200ddd48ca6
killa_bytes <lisa.villani@theguardian.com>
2018-03-13 16:03:04 +0000

    updates fallback url to new doc white-fright

--- src/js/lib/sheetNameFromShortId.js
+++ src/js/lib/sheetNameFromShortId.js
@@ -1,4 +1,4 @@
-const DEFAULT_DOC = 'how-to-make-a-pearl';
+const DEFAULT_DOC = 'white-fright';
 
 export default function sheetNameFromShortId(docsArray, shortUrl) {
     const map = new Map(docsArray);
