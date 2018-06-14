873ee1c6c4cb0afbf28d25ab04e431e794d107de
killa_bytes <lisa.villani@theguardian.com>
2018-06-14 13:39:56 +0100

    updates fallback url to the trap

--- src/js/lib/sheetNameFromShortId.js
+++ src/js/lib/sheetNameFromShortId.js
@@ -1,4 +1,4 @@
-const DEFAULT_DOC = 'the-tower-next-door';
+const DEFAULT_DOC = 'the-trap';
 
 export default function sheetNameFromShortId(docsArray, shortUrl) {
     const map = new Map(docsArray);
