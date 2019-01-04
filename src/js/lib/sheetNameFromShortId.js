09ce048c1a69d1221bf8f46be1bfe8f36301264f
killa_bytes <lisa.villani@theguardian.com>
2019-01-04 12:01:30 +0000

    changes fallback to fighting shame

--- src/js/lib/sheetNameFromShortId.js
+++ src/js/lib/sheetNameFromShortId.js
@@ -1,4 +1,4 @@
-const DEFAULT_DOC = 'marielle-and-monica';
+const DEFAULT_DOC = 'fighting-shame';
 
 export default function sheetNameFromShortId(docsArray, shortUrl) {
     const map = new Map(docsArray);
