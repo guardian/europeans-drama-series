b0ae1c6020e2f2176b162f45f0bf39fc200d8cdd
killa_bytes <lisa.villani@theguardian.com>
2019-02-05 12:26:08 +0000

    updates fallback url to the breadmaker

--- src/js/lib/sheetNameFromShortId.js
+++ src/js/lib/sheetNameFromShortId.js
@@ -1,4 +1,4 @@
-const DEFAULT_DOC = 'fighting-shame';
+const DEFAULT_DOC = 'the-breadmaker';
 
 export default function sheetNameFromShortId(docsArray, shortUrl) {
     const map = new Map(docsArray);
