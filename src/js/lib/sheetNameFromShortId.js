36a93b84638d9bf6acc3e7f361cac8e310951899
killa_bytes <lisa.villani@theguardian.com>
2019-03-25 15:26:34 +0000

    changed fallback url to someone-elses-war

--- src/js/lib/sheetNameFromShortId.js
+++ src/js/lib/sheetNameFromShortId.js
@@ -1,4 +1,4 @@
-const DEFAULT_DOC = 'where-the-river-runs-red';
+const DEFAULT_DOC = 'someone-elses-war';
 
 export default function sheetNameFromShortId(docsArray, shortUrl) {
     const map = new Map(docsArray);
