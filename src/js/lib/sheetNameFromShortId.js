bc14bd3a74fd17dc184c6b145982c6ff1110ef46
Duarte Carrilho da Graça <4561+duarte@users.noreply.github.com>
2019-12-02 15:37:48 +0100

    Updates preview harness

--- src/js/lib/sheetNameFromShortId.js
+++ src/js/lib/sheetNameFromShortId.js
@@ -1,7 +1,18 @@
-const DEFAULT_DOC = 'open-water';
+const DEFAULT_DOC = 'lost-rambos';
 
 export default function sheetNameFromShortId(docsArray, shortUrl) {
     const map = new Map(docsArray);
     const docName = map.get(shortUrl);
+    console.log("hihihihhihi")
+    console.log("hihihihhihi")
+    console.log(DEFAULT_DOC)
+    console.log("hihihihhihi")
+    console.log("hihihihhihi")
+
+    // uncomment and just return DEFAULT_DOC
+    // to test other docs locally
+    // return DEFAULT_DOC;
+
     return docName ? docName : DEFAULT_DOC;
+
 }
