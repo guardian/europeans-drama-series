01d03f4a8eff22001b1076c3d65f5ac588d2a8d6
Duarte Carrilho da Graça <4561+duarte@users.noreply.github.com>
2020-01-30 15:44:14 +0100

    use test data locally

--- src/js/lib/sheetURL.js
+++ src/js/lib/sheetURL.js
@@ -1,4 +1,9 @@
-export default function sheetURL(sheetID, type='docsdata') {
-    var protocol = window.location.protocol === 'file:' ? 'https://' : '//';
-    return `${protocol}interactive.guim.co.uk/${type}/${sheetID}.json`;
+export default function sheetURL(sheetID) {
+  var protocol = window.location.protocol === 'file:' ? 'https://' : '//';
+  if (window.location.hostname == 'localhost') {
+    var type = 'docsdata-test';
+  } else {
+    var type = 'docsdata';
+  }
+  return `${protocol}interactive.guim.co.uk/${type}/${sheetID}.json`;
 }
