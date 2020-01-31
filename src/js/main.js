831db931a7226caa8e33494a456182a5acf5ccad
Duarte Carrilho da Graça <4561+duarte@users.noreply.github.com>
2020-01-31 12:35:43 +0100

    adds a comment for the legacy doc-cards code

--- src/js/main.js
+++ src/js/main.js
@@ -128,11 +128,13 @@ export function init(el, context, config) {
         el.parentNode.replaceChild(builder, el);
 
         if (docData.linkedDocs.length > 0) {
+            // use linkedDocs data to inject onward journey elements
             docData.linkedDocs.forEach((linkedDoc, i) => {
                 const el = builder.querySelector(`section#more-documentaries .fc-slice__item:nth-child(${++i}) .nextSnap`);
                 el.innerHTML = `<div class="doc-card"><img class="doc-card__poster" src="${linkedDoc.posterImage}"><div class="doc-card__meta"><p class="doc-card__description">${linkedDoc.logline}</p><a class="doc-card__link" href="${linkedDoc.fullLink}"></a></div></div>`;
             });
         } else {
+            // Use legacy doc-cards snaps
             docData.onwardJourneyLinks.forEach(snapLink => {
                 reqwest({
                     'url': snapLink.jsonUrl,
