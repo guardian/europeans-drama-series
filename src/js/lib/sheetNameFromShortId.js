134c95f7b58032b7446b722116260e2762083662
Duarte Carrilho da Graça <4561+duarte@users.noreply.github.com>
2020-03-02 11:49:09 +0100

    third pass on image positions

--- src/js/lib/sheetNameFromShortId.js
+++ src/js/lib/sheetNameFromShortId.js
@@ -1,4 +1,10 @@
-const DEFAULT_DOC = 'poland';
+const DEFAULT_DOC = 'france';
+// const DEFAULT_DOC = 'poland';
+// const DEFAULT_DOC = 'uk';
+// const DEFAULT_DOC = 'spain';
+// const DEFAULT_DOC = 'germany';
+// const DEFAULT_DOC = 'sweden';
+// const DEFAULT_DOC = 'ireland';
 
 export default function sheetNameFromShortId(docsArray, shortUrl) {
     const map = new Map(docsArray);
